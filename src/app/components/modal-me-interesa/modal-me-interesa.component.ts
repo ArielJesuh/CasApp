5a1f18f1d9314c63288b6b1d510a598ea74cea1b
samcid <samuelcidbilbao@gmail.com>
2023-11-16 18:15:20 -0300

    mandar correo

--- src/app/components/modal-me-interesa/modal-me-interesa.component.ts
+++ src/app/components/modal-me-interesa/modal-me-interesa.component.ts
@@ -11,6 +11,7 @@ import { Usuario } from 'src/app/interfaces/usuario';
 export class ModalMeInteresaComponent {
   usuario: Usuario;
   nombre: string;
+  contenidoCorreo: string = '';
 
   constructor(
     public dialogRef: MatDialogRef<ModalMeInteresaComponent>,
@@ -22,5 +23,13 @@ export class ModalMeInteresaComponent {
 
   closeModal(): void {
     this.dialogRef.close();
+  }
+
+  generarEnlaceGmail(): string {
+    const destinoCorreo = encodeURIComponent(this.usuario.email);
+    const asunto = encodeURIComponent('Consulta sobre propiedad');
+    const cuerpo = encodeURIComponent(this.contenidoCorreo);
+
+    return `https://mail.google.com/mail/?view=cm&to=${destinoCorreo}&su=${asunto}&body=${cuerpo}`;
   }
 }
