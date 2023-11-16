cf7d3058b8324517088976bbb1a3f1c2660d7b7a
ArielJesuh <ari.cofre@duocuc.cl>
2023-11-16 16:10:36 -0300

    Home Mejorado
    Home mejorado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ViviendaComponent } from '../vivienda/vivienda.component';
+import { ViviendaService } from 'src/app/services/vivienda.service';
+import { Vivienda } from 'src/app/interfaces/vivienda';
 
 @Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild(ViviendaComponent) vivienda!:ViviendaComponent;
+  viviendasList: Vivienda[] = [];
+  viviendasListFix : Vivienda[] = [];
+  token:any;
+  largoLista :any;
+  constructor(private viviendaService: ViviendaService){
+    
+  }
+  
+  ngOnInit(): void {
+    this.getListViviendas()
+
+
+  }
+
+
+  getListViviendas(){
+    this.viviendaService.getListViviendas().subscribe((data: Vivienda[]) => {
+      this.viviendasList = data;
+    }
+  )
+  this.viviendasListFix = this.viviendasList.slice(0,3)
+
+}
+
+
+isLoggedIn():boolean{
+  this.token = localStorage.getItem("token");
+  if(this.token != null){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isDisconnected():boolean{
+  this.token = localStorage.getItem("token");
+  if(this.token != null){
+    return false;
+  } else {
+    return true;
+  }
+}
 
 }
